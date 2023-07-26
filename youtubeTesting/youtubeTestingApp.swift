04e096e828448319741cc9e44782adfcfe181c63
Enrique <enriqueg0604@gmail.com>
2023-07-25 19:45:55 -0500

    add inital files

---
+++ youtubeTesting/youtubeTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  youtubeTestingApp.swift
+//  youtubeTesting
+//
+//  Created by Enrique Garcia on 7/25/23.
+//
+
+import SwiftUI
+
+@main
+struct youtubeTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
